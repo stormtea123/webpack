9f00659fb8d3669e327b2f50a1dd56bdee1c164e
李金标 <hzlijb@tairanchina.com>
2016-09-08 17:44:10 +0800

    取消css单独提取版本

--- build.webpack.config.js
+++ build.webpack.config.js
@@ -1,22 +1,16 @@
 var webpack = require('webpack');
 var path = require('path');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var autoprefixer = require('autoprefixer');
 module.exports = [{
     name: "production",
     entry: {
-        user: "./src/user.js"
-        //goods:"./src/goods.js",
+        user: "./src/user.js",
+        welcome:"./src/welcome.js"
     },
     output: {
         path: __dirname,
         filename: "./build/[name].bundle.min.js"
     },
     plugins: [
-        new ExtractTextPlugin({
-            filename: "./build/[name].bundle.min.css",
-            allChunks: true
-        }),
         new webpack.optimize.UglifyJsPlugin({
             compress: {
                 warnings: false
@@ -27,21 +21,16 @@ module.exports = [{
         loaders: [{
             test: /\.html$/,
             loader: "html"
-        }, {
-            test: /\.css$/, loader: ExtractTextPlugin.extract({
-                fallbackLoader: "style-loader",
-                loader: "css-loader!postcss-loader"
-            })
+        },{
+            test: /\.css$/,
+            loader: "style-loader!css-loader"
         }, {
             test: /\.(png)$/,
             // inline base64 URLs for <=8k images, direct URLs for the rest
-            loader: 'url-loader?name=./build/images/[name].[ext]'
+            loader: 'url-loader?name=images/[name].[ext]'
         }, {
             test: /\.jpg$/,
-            loader: "file-loader?name=./build/images/[name].[ext]"
+            loader: "file-loader?name=images/[name].[ext]"
         }]
-    },
-    postcss: function () {
-        return [autoprefixer];
     }
 }]
--- dev.webpack.config.js
+++ dev.webpack.config.js
@@ -1,40 +1,27 @@
 var webpack = require('webpack');
 var path = require('path');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var autoprefixer = require('autoprefixer');
 module.exports = [{
     name: "develop",
     entry: {
-        user: "./src/user.js"
-        //goods:"./src/goods.js",
+        user: "./src/user.js",
+        welcome:"./src/welcome.js"
     },
     output: {
         path: __dirname,
         filename: "./build/[name].bundle.js"
     },
-    plugins: [
-        new ExtractTextPlugin({
-            filename: "./build/[name].bundle.css",
-            allChunks: true
-        })
-    ],
     module: {
         //提取css
         loaders: [{
             test: /\.html$/,
             loader: "html"
-        }, {
-            test: /\.css$/, loader: ExtractTextPlugin.extract({
-                fallbackLoader: "style-loader",
-                loader: "css-loader!postcss-loader"
-            })
+        },{
+            test: /\.css$/,
+            loader: "style-loader!css-loader"
         }, {
             test: /\.(png)$/,
             // inline base64 URLs for <=8k images, direct URLs for the rest
-            loader: 'url-loader?name=./build/images/[name].[ext]'
-        }, {test: /\.jpg$/, loader: "file-loader?name=./build/images/[name].[ext]"}]
-    },
-    postcss: function () {
-        return [autoprefixer];
+            loader: 'url-loader?name=images/[name].[ext]'
+        }, {test: /\.jpg$/, loader: "file-loader?name=images/[name].[ext]"}]
     }
 }]
